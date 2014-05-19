ed89cbc86e5cb5de611eda99e7d46ce026c18e97
Michael Herman <hermanmu@gmail.com>
2014-05-19 16:49:58 -0600

    updated js

--- project/assets/js/main.js
+++ project/assets/js/main.js
@@ -8,4 +8,24 @@ $(function() {
       gallery.toggleFullscreen(); // toggles the fullscreen
     });
   });
+
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', 'http://www.uscis.gov/files/nativedocuments/Track%2093.mp3');
+  audioElement.setAttribute('autoplay', 'autoplay');
+  //audioElement.load()
+  $.get();
+  audioElement.addEventListener("load", function() {
+  audioElement.play();
+  }, true);
+
+
+  $('.play').click(function() {
+    audioElement.play();
+  });
+
+
+  $('.pause').click(function() {
+    audioElement.pause();
+  });
+
 });
