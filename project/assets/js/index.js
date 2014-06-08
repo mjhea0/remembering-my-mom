604c5a32215b9a7416d9ef1961d78551cbf1fa0c
Michael Herman <hermanmu@gmail.com>
2014-06-08 15:48:03 -0500

    added audio mp3

--- project/assets/js/index.js
+++ project/assets/js/index.js
@@ -5,15 +5,7 @@ $(function() {
   audioElement.setAttribute('src', '/audio/mom_voicemail.mp3');
   // audioElement.setAttribute('autoplay', 'autoplay');
   //audioElement.load()
-  $.get();
-  audioElement.addEventListener("load", function() {
-    audioElement.play();
-    audioElement.loop=true;
-  }, true);
-  audioElement.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-  }, false);
+
 
   $('.play-icon').click(function() {
     audioElement.play();
